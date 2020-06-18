4d066d53e21fce92b0d769949334a7363f487b6e
Mark Heil <mark.heil1122@gmail.com>
2020-06-18 13:27:51 -0700

    worked on apiroutes.js

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,1 +1,42 @@
 let fs = require("fs");
+let db = require("../db/db.json");
+
+module.exports = function (app) {
+
+    app.get("api/notes", function (req, res) {
+        fs.readFile(".db/db.json", "utf8", (err, jsonString) => {
+            let noteData = JSON.parse(jsonString);
+        });
+    })
+
+    app.post("/api/notes", function (req, res) {
+        let noteId = uuidv4();
+        let newNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: noteId
+        }
+        fs.readFile("./db/db.json", "utf8", (err, jsonString) =>{
+            let noteData = JSON.parse(jsonString);
+            noteData.push(newNote);
+            fs.writeFile("./db/db.json", JSON.stringify(noteData, null, 2), err => {
+                if (err) throw err;
+                res.send(noteData);
+            });
+        });
+    });
+
+    app.delete("/api/notes/:id", function (req, res) {
+        let noteId = req.params.id;
+        fs.readFile("./db/db.json", "utf8", (err, jsonString) => {
+            let noteData = JSON.parse(jsonString);
+            let updatedNoteData = noteData.filter(note => note.id != noteId);
+            fs.writeFile("./db/db.json", JSON.stringify(updatedNoteData, null, 2), err => {
+                if (err) throw err;
+                res.send(updatedNoteData);
+            })
+        })
+    });
+
+
+};
